8444fdc00ac4f5f63b7ead232b73c63b53eb71bb
Fore7630 <licorey8@gmail.com>
2016-10-13 15:40:04 -0500

    Test
    I did this

---
+++ AnimalKingdom.java
@@ -0,0 +1,27 @@
+// Name:
+// VUnetID:
+// Email:
+// Class: CS 1101 - Vanderbilt University
+// Honor statement:
+// Date:
+
+// Description: This program prints animal faces.
+
+public class AnimalKingdom {
+
+    public static void main(String[] args) {
+        printMessage();
+
+        // add your code to print the various objects here
+
+    }
+
+    // prints a introductory message
+    public static void printMessage() {
+        System.out.println("This program will print various animal faces.");
+        System.out.println();
+    }
+
+    // add your helper methods here
+
+}
